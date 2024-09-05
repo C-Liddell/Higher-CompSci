3e63de59814b989dad3946ce4290a6f1b20d6e68
Cameron Liddell <180171652+C-Liddell@users.noreply.github.com>
2024-09-05 11:37:10 +0000

    .

---
+++ Decorators_Assistant.py
@@ -0,0 +1,7 @@
+def area(length, breadth, height):
+    
+
+length = int(input("Enter length of room: "))
+breadth = int(input("Enter breadth of room: "))
+height = int(input("Enter height of room: "))
+area(length, breadth, height)
