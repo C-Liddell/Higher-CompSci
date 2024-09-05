9d35c4fe5aff32bd6cb84264431577304836e593
Cameron Liddell <180171652+C-Liddell@users.noreply.github.com>
2024-09-05 11:46:55 +0000

    Further development

--- Decorators_Assistant.py
+++ Decorators_Assistant.py
@@ -1,7 +1,15 @@
-def area(length, breadth, height):
-    
+def user_input():
+    length = int(input("Enter length of room: "))
+    breadth = int(input("Enter breadth of room: "))
+    height = int(input("Enter height of room: "))
+    return(length, breadth, height)
 
-length = int(input("Enter length of room: "))
-breadth = int(input("Enter breadth of room: "))
-height = int(input("Enter height of room: "))
+def area(length, breadth, height):
+    floor_area = length*breadth
+    wall_area = 2*(height*length) + 2*(height*breadth)
+    return(floor_area, wall_area)
+
+def calculations():
+    pass
+user_input()
 area(length, breadth, height)
