aaccfaa27808626c241000360b5196efbcd47302
C-Liddell <c.liddell@albynschool.org>
2024-09-16 10:54:11 +0100

    .

---
+++ Python/Arrays/8011task2.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass
+class person():
+  height : float = 0.0
+  weight : float = 0.0
+  bmi : float = 0.0
+
+people = [person() for index in range(5)]
+
+for i in range(5):
+  people[i].height = float(input(f"Height {i+1}: "))
+  people[i].weight = float(input(f"Weight {i+1}: "))
+  people[i].bmi = people[i].weight/(people[i].height**2)
+  print(people[i])
+
+'''
+Task 2
+
+Create code which asks the user to enter the details of the following five people:
+
+Person 0: height = 1.75, weight = 70.5
+Person 1: height = 1.62, weight = 55.8
+Person 2: height = 1.88, weight = 95.2
+Person 3: height = 1.70, weight = 82.3
+Person 4: height = 1.80, weight = 68.0
+
+Remember to include the code to calculate the BMI of each person and display each record on the screen.
+
+You should use a fixed loop when creating your solution.
+'''
