9020c8a2287da0b159a3976ed81a6ef5d3926169
C-Liddell <c.liddell@albynschool.org>
2024-10-31 11:59:05 +0000

    .

--- Python/Pre-Defined Functions/1201 Task3.py
+++ Python/Pre-Defined Functions/1201 Task3.py
@@ -1,19 +1,23 @@
 def getPass():
-    password = str(input("Enter password: "))
+    password = str(input("Please enter a secure password: "))
     return password
 
 def checkCap(password):
     character = ord(password[0])
     if character > 64 and character < 91:
+        print("Your password begins with a capital letter. Good!")
         return True
     else:
+        print("This password is not secure. Try again.")
         return False
 
 def checkSymbol(password):
     character = ord(password[-1])
     if character > 34 and character < 38:
+        print("Your password ends with a symbol. Good!")
         return True
     else:
+        print("This password is not secure. Try again.")
         return False
     
 password = getPass()
@@ -21,5 +25,5 @@ password = getPass()
 while checkCap(password) != True or checkSymbol(password) != True:
     password = getPass()
 
-print("Success")
+print("This password is secure. Well done.")
 
