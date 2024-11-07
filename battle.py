02555d9d76e3aedfe910005143c0eae2802bcabc
C-Liddell <c.liddell@albynschool.org>
2024-11-07 12:45:35 +0000

    .

--- battle.py
+++ battle.py
@@ -12,23 +12,15 @@ class character:
     def __str__(self):
         return self.name
 
-player = character("player", 100, 5, 7, 6)
-monster = character("monster", 75, 6, 6, 5)
+player = character("Player", 100, 5, 7, 6)
+monster = character("Goblin", 75, 6, 6, 5)
 
-print("A monster approaches.")
+print(f"A {monster.name} approaches.")
 
 def main():
     mAction = rd.randint(1,3)
-    print("""
-          The Monster looms in front of you.
-          Would you like to...
+    pAction = int(input("Choose an action (1. Fight, 2. Block or 3. Dodge)(1, 2 or 3): "))
 
-          1. Fight
-          2. Block
-          3. Dodge
-
- """)
-    pAction = int(input("Your action: "))
     if pAction == 1:
         fight(mAction)
     if pAction == 2:
@@ -38,24 +30,21 @@ def main():
 
 def fight(mAction):
     if mAction == 1:
-        print("You and the monster both take a swing at each other.")
-        if getStrength(player) > getStrength(monster):
-            print("You slide by the monsters attack and strike it.")
-            pDamage = rd.randint(1,5) + player.damage
-            takeDamage(monster, pDamage)
+        print(f"You and the {monster} both try to fight.")
+        if skillCheck(player.speed) > skillCheck(monster.speed):
+            print(f"You slide by the {monster} strike it.")
+            damage(player, monster, 5)
         else:
-            print("The monster strikes you.")
-            mDamage = rd.randint(1,5) + monster.damage
-            takeDamage(player, mDamage)
+            print(f"The {monster} strikes you.")
+            damage(monster, player, 5)
+
     elif mAction == 2:
-        print("The monster tries to block your attack.")
-        if getStrength(monster) > getStrength(player):
-            print("The monster blocked you.")
+        print(f"The {monster} tries to block your attack.")
+        if skillCheck(monster.strength) > skillCheck(player.strength):
+            print(f"The {monster} blocked you.")
         else:
             print("You break through the block")
-            pDamage = rd.randint(1,5) + player.damage - (monster.strength/2)
-            takeDamage(monster, pDamage)
-    
+            damage(player, monster, 5) 
 
 def block():
     pass
@@ -63,13 +52,15 @@ def block():
 def dodge():
     pass
 
-def getStrength(name):
-    name = rd.randint(1,10) + name.strength
-    return name
+def skillCheck(type):
+    value = rd.randint(1,10) + type
+    return value
 
-def takeDamage(name, damage):
-    name.health -= damage
-    print(f"{name} took {damage}hp of damage.")
+def damage(attack, defence, mod):
+    mod = rd.randint(1, mod)
+    damage = mod + attack.damage
+    defence.health -= damage
+    print(f"{defence} took {damage}hp of damage.")
 
 while monster.health > 0:
     main()
