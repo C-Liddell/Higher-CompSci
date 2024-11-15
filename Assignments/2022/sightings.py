8fa762c808f8eab1ac6f197d6c6df7f86e34abfb
C-Liddell <c.liddell@albynschool.org>
2024-11-15 09:22:16 +0000

    .

--- Assignments/2022/sightings.py
+++ Assignments/2022/sightings.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 import csv
 
+
 @dataclass
 class sighting:
     town: str
     mammal: str
     date: str
     age: int
+
 
 def read():
     sightings = []
@@ -16,12 +18,14 @@ def read():
             sightings.append(sighting(i[0], i[1], i[2], int(i[3])))
     return sightings
 
-def findMaxAge():
+
+def findMaxAge(sightings):
     maxAge = 0
     for i in sightings:
         if i.age > maxAge:
             maxAge = i.age
-    print(maxAge)
+    print(f"The oldest walker was {maxAge}.")
+
 
 def findDates(sightings):
     town = capitalise(str(input("Enter town: ")))
@@ -31,14 +35,29 @@ def findDates(sightings):
         if i.town == town and i.mammal == mammal:
             print(i.date)
 
+
 def capitalise(string):
     character = ord(string[0])
-    character += 32
-    character = str(character)
+    if character >= 97 and character <= 122:
+        character = chr(character - 32)
+        string = character + string[1:len(string)]
+    return string
     
 
-    
+def findSightingsPerDay(sightings):
+    counter = 0
+    dayToCount = sightings[0].date
+    for i in sightings:
+        if i.date == dayToCount:
+            counter += 1
+        else:
+            print(f"Day to count: {dayToCount}, Count: {counter}")
+            dayToCount = i.date
+            counter = 1
+    print(f"Day to count: {dayToCount}, Count: {counter}")
+
 
 sightings = read()
 findMaxAge(sightings)
 findDates(sightings)
+findSightingsPerDay(sightings)
