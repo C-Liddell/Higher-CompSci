394d2462303228f764017fab6629aab6caa94bcc
C-Liddell <c.liddell@albynschool.org>
2025-03-03 10:58:28 +0000

    .

--- Assignments/2024/CEO.py
+++ Assignments/2024/CEO.py
@@ -9,9 +9,9 @@ def read():
     with open("Assignments/2024/companies.csv", "r") as file:
         data = csv.reader(file)
         for row in data:
-            company.append(row[0])
-            numEmployees.append(row[1])
-            ceoSalary.append(row[2])
+            company.append(str(row[0]))
+            numEmployees.append(int(row[1]))
+            ceoSalary.append(int(row[2]))
     return company, numEmployees, ceoSalary
 
 
@@ -26,19 +26,40 @@ def findDifference(company, ceoSalary):
     
     if found == True:
         difference = ceoSalary[highestSalaryPos] - ceoSalary[position]
-        print(f"The company with the highest CEO salary is {company[highestSalaryPos]}. The selected company was {company[position]}. The difference between CEO salaries was {difference}.")
+        print(f"""
+The company with the highest CEO salary is {company[highestSalaryPos]}. 
+The selected company was {company[position]}. 
+The difference between CEO salaries was {difference}.
+              """)
+    else:
+        print("Company not found.")
 
 
 
 def findMaxPos(list):
+    maxValue = 0
     maxPos = 0
     for i in range(0, len(list)):
-        if list[i] > max:
+        if list[i] > maxValue:
+            maxValue = list[i]
             maxPos = i
     return maxPos
 
 
 
+def countEmployees(numEmployees):
+    mostEmployeePos = findMaxPos(numEmployees)
+    count = 0
+    for i in range(0, len(numEmployees)):
+        if numEmployees[i] >= numEmployees[mostEmployeePos]*0.9:
+            count += 1
+    print(f"""
+The highest number of employees at a single company is {numEmployees[mostEmployeePos]}.
+The number of companies who employ within 10% of that figure is {count}.
+          """)
+
+
 
 company, numEmployees, ceoSalary = read()
 findDifference(company, ceoSalary)
+countEmployees(numEmployees)
