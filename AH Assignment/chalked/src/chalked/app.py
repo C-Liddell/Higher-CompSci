fd537428af65ac429c21aa92872d42521ee604a2
C-Liddell <c.liddell@albynschool.org>
2025-09-25 15:33:41 +0100

    .

--- AH Assignment/chalked/src/chalked/app.py
+++ AH Assignment/chalked/src/chalked/app.py
@@ -7,9 +7,9 @@ from toga.style.pack import COLUMN, ROW
 
 class Entry():
     def __init__(self):
-        self.__date = ""
-        self.__type = ""
-        self.__grade = ""
+        self.__date = "01/01/25"
+        self.__type = "Climb"
+        self.__grade = "V0"
         self.__attempts = 0
 
     def getDate(self):
@@ -32,10 +32,11 @@ class Entry():
     def increaseAttempts(self):
         self.__attempts += 1
 
-testEntry = Entry()
-testEntry.setDate("24/09/25")
-testEntry.setType("Boulder")
-testEntry.setGrade("V6")
+    def getDetails(self):
+        return f"{self.__type}, {self.__grade}, {self.__attempts}"
+
+entries = [Entry() for x in range(10)]
+
 
 
 class Chalked(toga.App):
@@ -46,20 +47,38 @@ class Chalked(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box(direction = COLUMN)
+        mainBox = toga.Box(direction = COLUMN)
+        searchBox = toga.Box(direction = ROW)
+        filterBox = toga.Box(direction = ROW)
+        listBox = toga.Box(direction = COLUMN)
+        navBox = toga.Box(direction = ROW)
+
+
+        searchBar = toga.TextInput(value = "Enter Search Term...", flex = 7)
+        searchButton = toga.Button("Search", flex = 1)
+
+        filter1 = toga.Button("Lead Climbs", flex = 1)
+        filter2 = toga.Button("Top Rope Climbs", flex = 1)
+        filter3 = toga.Button("Boulders", flex = 1)
 
         table = toga.DetailedList(
             data = [{
                 "icon": None,
-                "title": testEntry.getDate(),
-                "subtitle": f"{testEntry.getType()}, {testEntry.getGrade()}, {testEntry.getAttempts()}"
-            }]
+                "title": i.getDate(),
+                "subtitle": i.getDetails()
+                } for i in entries]
         )
 
-        main_box.add(table)
+        addButton = toga.Button("Add Entry")
+
+        mainBox.add(searchBox, filterBox, listBox, navBox)
+        searchBox.add(searchBar, searchButton)
+        filterBox.add(filter1, filter2, filter3)
+        listBox.add(table)
+        navBox.add(addButton)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = mainBox
         self.main_window.show()
 
 
